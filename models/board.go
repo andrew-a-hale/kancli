85d710af20cb08c61ee6ac8b7de0d5e9c5134a9f
Andrew Hale <andrew.hale003@gmail.com>
2024-04-27 11:05:40 +1000

    remove used help style

--- models/board.go
+++ models/board.go
@@ -25,8 +25,6 @@ var (
 			Padding(1, 2).
 			Border(lipgloss.RoundedBorder()).
 			BorderForeground(lipgloss.Color("62"))
-	helpStyle = lipgloss.NewStyle().
-			Foreground(lipgloss.Color("241"))
 )
 
 // CUSTOM ITEM TASK
